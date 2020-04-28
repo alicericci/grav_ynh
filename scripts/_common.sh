12f48f9dab15c73bd2f5ec12c99d5bf4e69c9af4
alice <dw@alice.local>
2020-04-28 17:25:02 +0200

    more test

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -13,3 +13,5 @@ exec_as() {
 		sudo -u "$USER" $@
 	fi
 }
+
+extra_pkg_dependencies="php7.2-fpm php7.2-cli php7.2-gd php7.2-mysql php7.2-xml php7.2-ldap php7.2-mbstring php7.2-json php7.2-simplexml php7.2-curl php7.2-mbstring php7.2-ctype php7.2-zip"
