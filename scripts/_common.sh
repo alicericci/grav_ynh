a86988aba8d3a04ac5c83f3b4ad9c4d6bcccb46a
alice <dw@alice.local>
2020-04-28 17:35:52 +0200

    morrre test

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,17 +1,26 @@
+# #!/bin/bash
+#
+# # Execute a command as another user
+# # usage: exec_as USER COMMAND [ARG ...]
+# exec_as() {
+# 	local USER=$1
+# 	shift 1
+#
+# 	if [[ $USER = $(whoami) ]]
+# 	then
+# 		eval $@
+# 	else
+# 		sudo -u "$USER" $@
+# 	fi
+# }
+
+
 #!/bin/bash
 
-# Execute a command as another user
-# usage: exec_as USER COMMAND [ARG ...]
-exec_as() {
-	local USER=$1
-	shift 1
+#=================================================
+# COMMON VARIABLES
+#=================================================
 
-	if [[ $USER = $(whoami) ]]
-	then
-		eval $@
-	else
-		sudo -u "$USER" $@
-	fi
-}
+# dependencies used by the app
 
 extra_pkg_dependencies="php7.2-fpm php7.2-cli php7.2-gd php7.2-mysql php7.2-xml php7.2-ldap php7.2-mbstring php7.2-json php7.2-simplexml php7.2-curl php7.2-mbstring php7.2-ctype php7.2-zip"
